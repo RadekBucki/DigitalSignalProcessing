f67956d12596e9bfccf17fa7a870da57c71437db
Radosław Bucki <236507@edu.p.lodz.pl>
2023-01-28 17:32:29 +0100

    Create signal operations

---
+++ src/main/java/backend/signal/AbstractSignal.java
@@ -0,0 +1,7 @@
+package backend.signal;
+
+import java.util.Map;
+
+public abstract class AbstractSignal {
+    public abstract Map<Double, Double> getAmplitudeFromTimeChartData();
+}
---
+++ src/main/java/backend/signal/DiscreteSignal.java
@@ -0,0 +1,12 @@
+package backend.signal;
+
+import java.util.HashMap;
+
+public class DiscreteSignal extends AbstractSignal {
+    public HashMap<Double, Double> getAmplitudeFromTimeChartData() {
+        return new HashMap<>();
+    }
+
+    public DiscreteSignal(HashMap<Double, Double> points) {
+    }
+}
