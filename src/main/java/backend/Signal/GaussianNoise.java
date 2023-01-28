6a318ec60a0b998ebec39de3efd25ac6d9fccdaf
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-01-28 17:44:38 +0100

    GaussianNoice

--- src/main/java/backend/Signal/GaussianNoise.java
+++ src/main/java/backend/Signal/GaussianNoise.java
@@ -1,7 +1,19 @@
 package backend.Signal;
 
+import java.util.Random;
+
 public class GaussianNoise extends ContinuousSignal {
+    private final Random r = new Random();
     public GaussianNoise(double a, double t1, double d) {
         super(a, t1, d);
+        int t1Rounded = (int) (t1 * pointsDecimalPlacesDivision);
+        int t2Rounded = (int) ((t1 + d) * pointsDecimalPlacesDivision);
+        for (int i = t1Rounded; i < t2Rounded; i++) {
+            points.put(i / pointsDecimalPlacesDivision, calculatePointValue());
+        }
+    }
+
+    private double calculatePointValue() {
+        return r.nextGaussian() * 2 * A - A;
     }
 }
