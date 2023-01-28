3cdd1300be8840908322aab4fbb42726310b0c5f
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-01-28 18:16:48 +0100

    SymmetricalRectangularSignal

--- src/main/java/backend/signal/SymmetricalRectangularSignal.java
+++ src/main/java/backend/signal/SymmetricalRectangularSignal.java
@@ -8,5 +8,19 @@ public class SymmetricalRectangularSignal extends ContinuousSignal {
         super(a, t1, d);
         this.T = T;
         this.kw = kw;
+        int t1Rounded = (int) (t1 * pointsDecimalPlacesDivision);
+        int t2Rounded = (int) ((t1 + d) * pointsDecimalPlacesDivision);
+        for (int i = t1Rounded; i < t2Rounded; i++) {
+            double pointX = i / pointsDecimalPlacesDivision;
+            points.put(pointX, calculatePointValue(pointX));
+        }
+    }
+
+    private double calculatePointValue(double x) {
+        int k = (int) Math.floor(x / T);
+        if (x >= k * T + t1 && x < kw * T + k * T + t1) {
+            return A;
+        }
+        return -A;
     }
 }
