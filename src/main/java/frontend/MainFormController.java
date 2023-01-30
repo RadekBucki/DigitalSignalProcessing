956a035c4b3a07b1040da6bb68e1ba08852e200f
Radosław Bucki <236507@edu.p.lodz.pl>
2023-01-30 14:13:25 +0100

    Refactor

--- src/main/java/frontend/MainFormController.java
+++ src/main/java/frontend/MainFormController.java
@@ -8,10 +8,7 @@ import frontend.fields.FieldsReader;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
@@ -23,68 +20,86 @@ public class MainFormController implements Initializable {
     public static final String MAIN_FORM_TITLE = "Digital Signal Processing";
     private final SignalFacade facade = new SignalFacade();
 
-    public final List<TextField> list = new ArrayList<>();
-    AbstractSignal signal;
-    Class<?> selectedComboBoxKey;
+    private final List<TextField> parametersTextFields = new ArrayList<>();
+    private AbstractSignal signal;
+    private Class<?> selectedComboBoxKey;
     @FXML
-    public GridPane parametersGrid;
+    private GridPane parametersGrid;
     @FXML
-    public ComboBox<String> signalTypes;
+    private ComboBox<String> signalTypes;
+    @FXML
+    private Button generateButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         createParametersTextFields(facade.getDefaultSignal());
         Map<Class<?>, String> signals = facade.getPossibleSignals().stream().collect(
-                Collectors.toMap(
-                        key -> key,
-                        ClassesTranslator::translatePascalCaseClassToText
-                )
+                Collectors.toMap(key -> key, ClassesTranslator::translatePascalCaseClassToText)
         );
         signalTypes.getItems().addAll(signals.values());
         signalTypes.setOnAction(event -> {
-            String selectedValue = signalTypes.getValue();
-            for (Map.Entry<Class<?>, String> entry : signals.entrySet()) {
-                if (selectedValue.equals(entry.getValue())) {
-                    createParametersTextFields(entry.getKey());
-                    selectedComboBoxKey = entry.getKey();
-                    break;
-                }
-            }
+            generateButton.setDisable(true);
+            signal = null;
+            Class<?> selectedKey = signals.entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue().equals(signalTypes.getValue()))
+                    .map(Map.Entry::getKey)
+                    .findFirst()
+                    .orElse(null);
+            createParametersTextFields(selectedKey);
+            selectedComboBoxKey = selectedKey;
         });
     }
 
-    public void createParametersTextFields(Class<?> classInstance) {
-        list.clear();
-        List<String> names = FieldsReader.getFieldNames(classInstance);
+    public void createParametersTextFields(Class<?> classDefinition) {
+        parametersTextFields.clear();
+        List<String> names = FieldsReader.getFieldNames(classDefinition);
         parametersGrid.getChildren().clear();
         for (int i = 0; i < names.size(); i++) {
             Group group = new Group();
-
-            TextField textField = new TextField();
-            textField.setLayoutX(334);
-            textField.setTextFormatter(new TextFormatter<>(c -> {
-                String newText = c.getControlNewText().replace(",", ".");
-                if (!newText.matches("-?([0-9]*[.])?[0-9]*")) {
-                    textField.clear();
-                    return null;
-                }
-                return c;
-            }));
-
-            Label label = new Label(FieldsMapper.map(names.get(i)));
-            label.setLabelFor(textField);
-            label.setLayoutY(4);
-
+            TextField textField = createGroupNumericalTextField();
+            Label label = createGroupLabel(FieldsMapper.map(names.get(i)), textField);
             group.getChildren().addAll(textField, label);
             parametersGrid.addRow(i, group);
-            list.add(textField);
+            parametersTextFields.add(textField);
         }
     }
 
     public void createSignalInstance() {
-        List<Double> values = list.stream().map(
+        List<Double> values = parametersTextFields.stream().map(
                 textField -> Double.parseDouble(textField.getText())
         ).toList();
         signal = facade.getSignal(selectedComboBoxKey, values);
+    }
+    private TextField createGroupNumericalTextField() {
+        TextField textField = new TextField();
+        textField.setLayoutX(334);
+        textField.setTextFormatter(new TextFormatter<>(text -> {
+            String newText = text.getControlNewText().replace(",", ".");
+            if (!newText.matches("-?([0-9]*[.])?[0-9]*")) {
+                textField.clear();
+                return null;
+            }
+            generateButton.setDisable(shouldGenerateButtonBeDisabled());
+            return text;
+        }));
+        return textField;
+    }
+    private Label createGroupLabel(String text, TextField labelFor) {
+        Label label = new Label(text);
+        label.setLabelFor(labelFor);
+        label.setLayoutY(4);
+        return label;
+    }
+    private boolean shouldGenerateButtonBeDisabled() {
+        if (selectedComboBoxKey == null) {
+            return true;
+        }
+        for (TextField textField : parametersTextFields) {
+            if (textField.getText().isEmpty()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
