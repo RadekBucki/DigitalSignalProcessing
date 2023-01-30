96191bc88c0ec75e5923fb1f812e65f64671f566
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-01-30 21:38:16 +0100

    Refactor - remove TextField list from mainController

--- src/main/java/frontend/MainFormController.java
+++ src/main/java/frontend/MainFormController.java
@@ -5,22 +5,26 @@ import backend.signal.AbstractSignal;
 import frontend.classes.ClassTranslator;
 import frontend.fields.FieldMapper;
 import frontend.fields.FieldReader;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import org.w3c.dom.Text;
 
 import java.net.URL;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
 
 public class MainFormController implements Initializable {
     public static final String MAIN_FORM_RESOURCE = "MainForm.fxml";
     public static final String MAIN_FORM_TITLE = "Digital Signal Processing";
     private final SignalFacade facade = new SignalFacade();
-
-    private final List<TextField> parametersTextFields = new ArrayList<>();
     private AbstractSignal signal;
     private Class<?> selectedComboBoxKey;
     @FXML
@@ -52,7 +56,6 @@ public class MainFormController implements Initializable {
     }
 
     public void createParametersTextFields(Class<?> classDefinition) {
-        parametersTextFields.clear();
         List<String> names = FieldReader.getFieldNames(classDefinition);
         parametersGrid.getChildren().clear();
         for (int i = 0; i < names.size(); i++) {
@@ -61,16 +64,16 @@ public class MainFormController implements Initializable {
             Label label = createGroupLabel(FieldMapper.map(names.get(i)), textField);
             group.getChildren().addAll(textField, label);
             parametersGrid.addRow(i, group);
-            parametersTextFields.add(textField);
         }
     }
 
     public void createSignalInstance() {
-        List<Double> values = parametersTextFields.stream().map(
-                textField -> Double.parseDouble(textField.getText())
-        ).toList();
+        List<Double> values = getParamsTextFieldsStream()
+                .map(textField -> Double.parseDouble(textField.getText()))
+                .toList();
         signal = facade.getSignal(selectedComboBoxKey, values);
     }
+
     private TextField createGroupNumericalTextField() {
         TextField textField = new TextField();
         textField.setLayoutX(334);
@@ -85,17 +88,24 @@ public class MainFormController implements Initializable {
         }));
         return textField;
     }
+
     private Label createGroupLabel(String text, TextField labelFor) {
         Label label = new Label(text);
         label.setLabelFor(labelFor);
-        label.setLayoutY(4);
         return label;
     }
+
+    private Stream<TextField> getParamsTextFieldsStream() {
+        return parametersGrid.getChildren()
+                .stream()
+                .map(group -> (TextField) ((Group) group).getChildren().get(0));
+    }
+
     private boolean shouldGenerateButtonBeDisabled() {
         if (selectedComboBoxKey == null) {
             return true;
         }
-        for (TextField textField : parametersTextFields) {
+        for (TextField textField : getParamsTextFieldsStream().toList()) {
             if (textField.getText().isEmpty()) {
                 return true;
             }
