b3432ec0b317297410c3f24135f2064ab8ac22d3
Radosław Bucki <236507@edu.p.lodz.pl>
2023-02-02 23:51:12 +0100

    Add signal operation tab

--- src/main/java/frontend/MainApplication.java
+++ src/main/java/frontend/MainApplication.java
@@ -10,7 +10,10 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
+
 import static frontend.MainFormController.*;
+import static frontend.SignalOperationFormController.SIGNAL_OPERATION_FORM_RESOURCE;
 
 /**
  * =========================================================
@@ -21,19 +24,24 @@ import static frontend.MainFormController.*;
 
 public class MainApplication extends Application {
     private static final int DEFAULT_CARD_NUMBER = 2;
+    private static final String MAIN_APPLICATION_STYLE = "MainApplication.css";
     private final TabPane tabPane = new TabPane();
     @Override
     public void start(Stage stage) throws IOException {
-        Label label = new Label(MAIN_FORM_TITLE);
-        label.setFont(new Font(24.0));
-        VBox vBox = new VBox(label, tabPane);
+        tabPane.getTabs().add(createSignalOperationTab());
         for (int i = 0; i < DEFAULT_CARD_NUMBER; i++) {
             Tab tab = createTab(i + 1);
             tabPane.getTabs().add(tab);
         }
         tabPane.getTabs().add(createAddTabButton());
+        tabPane.getSelectionModel().select(tabPane.getTabs().get(1));
 
-        Scene scene = new Scene(vBox);
+        Scene scene = new Scene(new VBox(createTitleLabel(), tabPane));
+        scene.getStylesheets()
+                .add(
+                        Objects.requireNonNull(getClass().getResource(MAIN_APPLICATION_STYLE))
+                                .toExternalForm()
+                );
         stage.setTitle(MAIN_FORM_TITLE);
         stage.setScene(scene);
         stage.show();
@@ -52,13 +60,14 @@ public class MainApplication extends Application {
         Tab addTab = new Tab();
         addTab.setText("+");
         addTab.setClosable(false);
+        addTab.getStyleClass().add("highlighting-tab");
         addTab.setOnSelectionChanged(event -> {
             if (!addTab.isSelected()) {
                 return;
             }
             Tab tab;
             try {
-                tab = createTab(tabPane.getTabs().size());
+                tab = createTab(tabPane.getTabs().size() - 1);
             } catch (IOException e) {
                 return;
             }
@@ -66,6 +75,22 @@ public class MainApplication extends Application {
             tabPane.getSelectionModel().select(tab);
         });
         return addTab;
+    }
+
+    private Tab createSignalOperationTab() throws IOException {
+        Tab tab = new Tab();
+        tab.setText("Signal Operation");
+        tab.setClosable(false);
+        tab.getStyleClass().add("highlighting-tab");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(SIGNAL_OPERATION_FORM_RESOURCE));
+        tab.setContent(fxmlLoader.load());
+        return tab;
+    }
+
+    private Label createTitleLabel() {
+        Label label = new Label(MAIN_FORM_TITLE);
+        label.setFont(new Font(24.0));
+        return label;
     }
     public static void main(String[] args) {
         launch();
---
+++ src/main/java/frontend/SignalOperationFormController.java
@@ -0,0 +1,6 @@
+package frontend;
+
+public class SignalOperationFormController {
+    public static final String SIGNAL_OPERATION_FORM_RESOURCE = "SignalOperationForm.fxml";
+    public static final String SIGNAL_OPERATION_FORM_TITLE = "Digital Signal Processing";
+}
