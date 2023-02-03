dc1afd6d0a7f00561ae7b7c752f01143cfa74de4
Radosław Bucki <236507@edu.p.lodz.pl>
2023-02-03 11:50:31 +0100

    Add apply button

--- src/main/java/frontend/SignalOperationTabController.java
+++ src/main/java/frontend/SignalOperationTabController.java
@@ -4,6 +4,7 @@ import backend.SignalFacade;
 import backend.signal.AbstractSignal;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
 import java.net.URL;
@@ -15,6 +16,8 @@ import java.util.function.BiFunction;
 public class SignalOperationTabController implements Initializable {
     public static final String RESOURCE = "SignalOperationTab.fxml";
     @FXML
+    private Button applyOperationButton;
+    @FXML
     private ComboBox<String> signalOperationComboBox;
     @FXML
     private ComboBox<String> signal1ComboBox;
@@ -22,22 +25,30 @@ public class SignalOperationTabController implements Initializable {
     private ComboBox<String> signal2ComboBox;
     private final SignalFacade signalFacade = new SignalFacade();
 
-    private final Map<BiFunction<AbstractSignal, AbstractSignal, AbstractSignal>, String> signalOperations = Map.of(
-            signalFacade::add, "Add",
-            signalFacade::subtract, "Subtract",
-            signalFacade::multiply, "Multiply",
-            signalFacade::divide, "Divide"
+    private final Map<String, BiFunction<AbstractSignal, AbstractSignal, AbstractSignal>>  signalOperations = Map.of(
+            "Add", signalFacade::add,
+            "Subtract", signalFacade::subtract,
+            "Multiply", signalFacade::multiply,
+            "Divide", signalFacade::divide
     );
     private final Map<String, AbstractSignal> signals = new LinkedHashMap<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        signalOperationComboBox.getItems().addAll(signalOperations.values());
+        signalOperationComboBox.getItems().addAll(signalOperations.keySet());
     }
 
     public void addOrUpdateSignal(String name, AbstractSignal signal) {
         signals.put(name, signal);
         signal1ComboBox.getItems().setAll(signals.keySet());
         signal2ComboBox.getItems().setAll(signals.keySet());
+    }
+
+    public void applyOperation() {
+        AbstractSignal signal = signalOperations.get(signalOperationComboBox.getValue()).apply(
+            signals.get(signal1ComboBox.getValue()),
+            signals.get(signal2ComboBox.getValue())
+        );
+        // TODO: Create new tab and set signal
     }
 }
