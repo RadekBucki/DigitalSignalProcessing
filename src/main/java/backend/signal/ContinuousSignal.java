3b305aad872cc6409b19e6b26f0c7e4829ecce4e
Radosław Bucki <236507@edu.p.lodz.pl>
2023-02-04 21:54:53 +0100

    Sonar issues

--- src/main/java/backend/signal/ContinuousSignal.java
+++ src/main/java/backend/signal/ContinuousSignal.java
@@ -50,19 +50,19 @@ public class ContinuousSignal extends AbstractSignal {
     @Override
     public double getAveragePower() {
         return (1 / (t2 - t1)) * si.integrate(Integer.MAX_VALUE,
-                (x) -> calculatePointValue(x) * calculatePointValue(x), t1, t2);
+                x -> calculatePointValue(x) * calculatePointValue(x), t1, t2);
     }
 
     @Override
     public double getVariance() {
         return (1 / (t2 - t1)) * si.integrate(Integer.MAX_VALUE,
-                (x) -> (calculatePointValue(x) - getAverage()) * (calculatePointValue(x) - getAverage()), t1, t2);
+                x -> (calculatePointValue(x) - getAverage()) * (calculatePointValue(x) - getAverage()), t1, t2);
     }
 
     @Override
     public double getEffectiveValue() {
         return Math.sqrt((1 / (t2 - t1)) * si.integrate(Integer.MAX_VALUE,
-                (x) -> calculatePointValue(x) * calculatePointValue(x), t1, t2));
+                x -> calculatePointValue(x) * calculatePointValue(x), t1, t2));
     }
 
     @Override
