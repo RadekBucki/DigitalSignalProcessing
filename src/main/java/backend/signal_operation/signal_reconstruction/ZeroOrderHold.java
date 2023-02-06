a324b1b02cc165bb8383b70db4e40311fdbd1598
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-02-06 19:24:37 +0100

    ZeroOrder bug fix

--- src/main/java/backend/signal_operation/signal_reconstruction/ZeroOrderHold.java
+++ src/main/java/backend/signal_operation/signal_reconstruction/ZeroOrderHold.java
@@ -9,7 +9,8 @@ public class ZeroOrderHold implements ReconstructMethod {
     @Override
     public double reconstruct(DiscreteSignal signal, double time) {
         return signal.getPoints().entrySet().stream()
-                .min(Comparator.comparingDouble(entry -> Math.abs(time - entry.getKey())))
+                .filter(x -> x.getKey() <= time)
+                .min(Comparator.comparingDouble(entry -> time - entry.getKey()))
                 .map(Map.Entry::getValue)
                 .orElseThrow();
     }
