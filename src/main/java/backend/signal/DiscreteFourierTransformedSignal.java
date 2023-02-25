a6dbb27ab29195cf912b1217b29634f7f6f8857c
Radosław Bucki <236507@edu.p.lodz.pl>
2023-02-25 19:42:35 +0100

    #90 Replace double with complex

--- src/main/java/backend/signal/DiscreteFourierTransformedSignal.java
+++ src/main/java/backend/signal/DiscreteFourierTransformedSignal.java
@@ -1,9 +1,13 @@
 package backend.signal;
 
+import org.apache.commons.math3.complex.Complex;
+
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DiscreteFourierTransformedSignal extends DiscreteSignal {
     protected Map<Double, Double> imaginaryPartPoints;
+
     public DiscreteFourierTransformedSignal(double A, double d, double f) {
         super(A, d, f);
     }
@@ -12,16 +16,31 @@ public class DiscreteFourierTransformedSignal extends DiscreteSignal {
         super(A, d, f, t1);
     }
 
-    public DiscreteFourierTransformedSignal(
-            Map<Double, Double> realPartPoints,
-            Map<Double, Double> imaginaryPartPoints
-    ) {
-        super(realPartPoints);
-        this.imaginaryPartPoints = imaginaryPartPoints;
+    public DiscreteFourierTransformedSignal(Map<Double, Complex> points) {
+        super(
+                points.entrySet()
+                        .stream()
+                        .collect(
+                                Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        entry -> entry.getValue().getReal()
+                                )
+                        )
+        );
+        this.imaginaryPartPoints = points.entrySet()
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> entry.getValue().getImaginary()
+                        )
+                );
     }
+
     public Map<Double, Double> getRealPartPoints() {
         return points;
     }
+
     public Map<Double, Double> getImaginaryPartPoints() {
         return imaginaryPartPoints;
     }
