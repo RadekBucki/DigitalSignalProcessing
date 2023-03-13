94df7fbeace55beb3fdb572b5b9fcbaa9e3d485e
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-03-13 21:07:09 +0100

    Sinc fix

--- src/main/java/backend/signal_operation/signal_reconstruction/Sinc.java
+++ src/main/java/backend/signal_operation/signal_reconstruction/Sinc.java
@@ -6,17 +6,19 @@ public class Sinc implements ReconstructMethod {
     int numOfSamples;
 
     public Sinc(int numOfSamples) {
-        this.numOfSamples = numOfSamples;
+        this.numOfSamples = numOfSamples % 2 == 1 ? numOfSamples - 1 : numOfSamples;
     }
 
     @Override
     public double reconstruct(DiscreteSignal signal, double time, double frequency) {
         int currentN = (int) Math.round(time * frequency);
-        int distanceToN1 = Math.min((currentN - signal.getN1()), 10);
-        int distanceToN2 = Math.min((currentN - signal.getN2()), 10);
+        final int toStart = (numOfSamples / 2) - (currentN - signal.getN1());
+        final int toEnd = (numOfSamples / 2) - (signal.getN2() - currentN);
         return signal.getPoints().entrySet().stream()
-                .filter(entry -> Math.round(entry.getKey() * frequency) <= currentN + (numOfSamples / 2.0) + 10 - distanceToN1)
-                .filter(entry -> Math.round(entry.getKey() * frequency) >= currentN - (numOfSamples / 2.0) - 10 + distanceToN2)
+                .filter(entry -> Math.round(entry.getKey() * frequency) <= currentN + (numOfSamples / 2.0) +
+                        (Math.max(toStart, 0)))
+                .filter(entry -> Math.round(entry.getKey() * frequency) >= currentN - (numOfSamples / 2.0) -
+                        Math.max(toEnd, 0))
                 .mapToDouble(entry -> entry.getValue() *
                         sinc(time / (1 / frequency) - Math.round(entry.getKey() * frequency)))
                 .sum();
