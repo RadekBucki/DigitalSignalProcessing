f113cdd02d1f072b92a0a9a4f8c873b1c7aef590
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-03-27 14:48:54 +0200

    Bug fix sinc
    Sinc fix

--- src/main/java/backend/signal_operation/signal_reconstruction/Sinc.java
+++ src/main/java/backend/signal_operation/signal_reconstruction/Sinc.java
@@ -6,19 +6,14 @@ public class Sinc implements ReconstructMethod {
     int numOfSamples;
 
     public Sinc(int numOfSamples) {
-        this.numOfSamples = numOfSamples % 2 == 1 ? numOfSamples - 1 : numOfSamples;
+        this.numOfSamples = numOfSamples;
     }
 
     @Override
     public double reconstruct(DiscreteSignal signal, double time, double frequency) {
-        int currentN = (int) Math.round(time * frequency);
-        final int toStart = (numOfSamples / 2) - (currentN - signal.getN1());
-        final int toEnd = (numOfSamples / 2) - (signal.getN2() - currentN);
         return signal.getPoints().entrySet().stream()
-                .filter(entry -> Math.round(entry.getKey() * frequency) <= currentN + (numOfSamples / 2.0) +
-                        (Math.max(toStart, 0)))
-                .filter(entry -> Math.round(entry.getKey() * frequency) >= currentN - (numOfSamples / 2.0) -
-                        Math.max(toEnd, 0))
+                .filter(entry -> entry.getKey() < time + ((1 / frequency) * numOfSamples))
+                .filter(entry -> entry.getKey() > time - ((1 / frequency) * numOfSamples))
                 .mapToDouble(entry -> entry.getValue() *
                         sinc(time / (1 / frequency) - Math.round(entry.getKey() * frequency)))
                 .sum();
